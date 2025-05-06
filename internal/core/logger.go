66619cac36d5eae540384208e6984117a31436a6
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-05-06 18:50:35 +0200

    feat: add asynchronous logger with buffered channel and worker goroutine

--- internal/core/logger.go
+++ internal/core/logger.go
@@ -5,15 +5,35 @@ import (
 	"os"
 )
 
+type Logger struct {
+	Level   string
+	Message string
+	Error   error
+}
+
+var logCh = make(chan Logger, 100)
+
 var (
 	infoLogger  = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	errorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
+func init() {
+	go func() {
+		for entry := range logCh {
+			if entry.Level == "info" {
+				infoLogger.Println(entry.Message)
+			} else if entry.Level == "error" && entry.Error != nil {
+				errorLogger.Println(entry.Error)
+			}
+		}
+	}()
+}
+
 func LogInfo(msg string) {
-	infoLogger.Println(msg)
+	logCh <- Logger{Level: "info", Message: msg}
 }
 
 func LogError(err error) {
-	errorLogger.Println(err)
+	logCh <- Logger{Level: "error", Error: err}
 }
